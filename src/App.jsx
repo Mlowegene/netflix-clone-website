7f8a86123e11dc4586b42fd65fc70885d670b829
Mlowegene <genemlowe@gmail.com>
2023-11-13 16:37:26 +0300

    create row component and request movies from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Row from "./components/Row";
+import requests from "./request";
+
 function App() {
   
   return (
-    <>
-      <h1>HEy mlowe</h1>
-    </>
+    <div className="App">
+      <h1>Netflix</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    </div>
   );
 }
 
